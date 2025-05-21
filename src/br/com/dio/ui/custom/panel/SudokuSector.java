555c0379ff4f8623ab0070096677e54b2f17cf4e
unknown <douglas.ferreirag@gmail.com>
2025-05-20 23:12:46 -0300

    Montagem da tela

--- src/br/com/dio/ui/custom/panel/SudokuSector.java
+++ src/br/com/dio/ui/custom/panel/SudokuSector.java
@@ -2,6 +2,8 @@ package br.com.dio.ui.custom.panel;
 
 //import br.com.dio.ui.custom.input.NumberText;
 
+import br.com.dio.ui.custom.input.NumberText;
+
 import javax.swing.JPanel;
 import javax.swing.border.LineBorder;
 import java.awt.Dimension;
@@ -11,13 +13,13 @@ import static java.awt.Color.black;
 
 public class SudokuSector extends JPanel {
 
-    public SudokuSector(){//(final List<NumberText> textFields){
+    public SudokuSector(final List<NumberText> textFields){
         var dimension = new Dimension(170, 170);
         this.setSize(dimension);
         this.setPreferredSize(dimension);
         this.setBorder(new LineBorder(black, 2, true));
         this.setVisible(true);
-       // textFields.forEach(this::add);
+        textFields.forEach(this::add);
     }
 
 }
